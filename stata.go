5ed158361df553b730d401f99252a2ec9233776c
DrGo <salah.mahmud@gmail.com>
2025-01-25 00:25:58 -0700

    added record appending

--- stata.go
+++ stata.go
@@ -1,7 +1,7 @@
-//Package stata writes data into a Stata 113 format (readable by any Stata version higher than 7)
-//Source for format info https://www.stata.com/help.cgi?dta_113
-//The package does not do much validation. It is up to the user to ensure that the supplied data
-//meets the format specification!
+// Package stata writes data into a Stata 113 format (readable by any Stata version higher than 7)
+// Source for format info https://www.stata.com/help.cgi?dta_113
+// The package does not do much validation. It is up to the user to ensure that the supplied data
+// meets the format specification!
 package gostata
 
 import (
@@ -23,13 +23,13 @@ const (
 	STATA_SHORTINT_NA = 32767
 	STATA_INT_NA      = 2147483647
 
-    // DTA113MaxInt8     int8   = 0x64       // 100
-    // DTA113MaxInt16    int16  = 0x7fe4     // 32740
-    // DTA113MaxInt32    int32  = 0x7fffffe4 // 2147483620
-    //
-    // DTA113MissingInt8  int8  = 0x65       // 101
-    // DTA113MissingInt16 int16 = 0x7FE5     // 32741
-    // DTA113MissingInt32 int32 = 0x7FFFFFE5 // 2147483621
+	// DTA113MaxInt8     int8   = 0x64       // 100
+	// DTA113MaxInt16    int16  = 0x7fe4     // 32740
+	// DTA113MaxInt32    int32  = 0x7fffffe4 // 2147483620
+	//
+	// DTA113MissingInt8  int8  = 0x65       // 101
+	// DTA113MissingInt16 int16 = 0x7FE5     // 32741
+	// DTA113MissingInt32 int32 = 0x7FFFFFE5 // 2147483621
 
 )
 
@@ -37,12 +37,12 @@ var (
 	STATA_FLOAT_NA  = math.Pow(2.0, 127)
 	STATA_DOUBLE_NA = math.Pow(2.0, 1023)
 
-	DTA113MaxFloat32 = math.Float32frombits(0x7effffff)        // ~1.7e38
-    DTA113MaxFloat64 = math.Float64frombits(0x7fdfffffffffffff) // ~8.9e307
-    
-    DTA113MissingFloat32 = math.Float32frombits(0x7F000000)
-    DTA113MissingFloat64 = math.Float64frombits(0x7FE0000000000000)
-
+	// DTA113MaxFloat32 = math.Float32frombits(0x7effffff)        // ~1.7e38
+	//    DTA113MaxFloat64 = math.Float64frombits(0x7fdfffffffffffff) // ~8.9e307
+	//
+	//    DTA113MissingFloat32 = math.Float32frombits(0x7F000000)
+	//    DTA113MissingFloat64 = math.Float64frombits(0x7FE0000000000000)
+	//
 	littleEndian = binary.LittleEndian //only write LittleEndian
 )
 
@@ -52,7 +52,7 @@ type (
 	stataLabel   [stataLabelSize]byte
 )
 
-//Supported Stata var types aliased to Go types for maximum convertability
+// Supported Stata var types aliased to Go types for maximum convertability
 type (
 	Byte   = int8
 	Int    = int16
@@ -61,7 +61,7 @@ type (
 	Double = float64
 )
 
-//Supported Stata variable types
+// Supported Stata variable types
 const (
 	StataByteId   = 251 // 0xfb
 	StataIntId    = 252 // 0xfc
@@ -85,7 +85,7 @@ const (
 				--------------------
 */
 
-//Field holds information on a Stata variable
+// Field holds information on a Stata variable
 type Field struct {
 	Name      string
 	FieldType byte
@@ -94,15 +94,15 @@ type Field struct {
 	data      interface{}
 }
 
-//field name must be exported for package Binary to see them
+// field name must be exported for package Binary to see them
 type header struct {
 	//	Contents            Length    Format    Comments
 	Version   byte       //     1    byte      contains 113 = 0x71
 	ByteOrder byte       //     1    byte      0x01 -> HILO, 0x02 -> LOHI
 	FileType  byte       //     1    byte      0x01
 	UnUsed    byte       //     1    byte      0x01
-	NoVar     int16      //		2    int       (number of vars) encoded per byteorder
-	NoObs     int32      // 		4    int       (number of obs)  encoded per byteorder
+	NumVars   int16      //		2    int       (number of vars) encoded per byteorder
+	NumObs    int32      // 		4    int       (number of obs)  encoded per byteorder
 	DataLabel stataLabel // 		81    char      dataset label, \0 terminated
 	TimeStamp [18]byte   // 		18    char      date/time saved, \0 terminated
 	//	Total                  109
@@ -117,15 +117,21 @@ func NewHeader() *header {
 	}
 	//FIXME: leave empty for production; comment the line below
 	copy(fh.DataLabel[:], "Written by VDEC Stata File Creator")
+	//FIXME: ? add timestamp
 	// fh.timeStamp[0] = 0
 	return &fh
 }
 
-//File Stata file info
+// File Stata file info
 type File struct {
 	*header
 	fields     []*Field
 	recordSize int
+	recBuf     []byte // buf for record appending
+	offset     int    //offset within the record buffer
+	f          *os.File
+	w          *bufio.Writer
+	internal_w bool //did we create w from a filename?
 	//FIXME: remove from the struct and just declare when needed?
 	//	Contents            	Length    	  Format       Comments
 	typList  []byte         //         nvar    byte array
@@ -136,7 +142,7 @@ type File struct {
 	vlblList []stataLabel
 }
 
-//NewFile returns a pointer to an initialized File.
+// NewFile returns a pointer to an initialized File.
 func NewFile() *File {
 	sf := File{
 		header: NewHeader(),
@@ -144,10 +150,10 @@ func NewFile() *File {
 	return &sf
 }
 
-//AddField adds a field to be written out to a Stata file
-//It does not verify similarly-named field does not exist
-//It does not verify field names and labels meet Stata requirements
-//It does not verify that slice lengths are identical
+// AddField adds a field to be written out to a Stata file
+// It does not verify similarly-named field does not exist
+// It does not verify field names and labels meet Stata requirements
+// It does not verify that slice lengths are identical
 func (sf *File) AddField(name, label string, slice interface{}) *Field {
 	var (
 		typ      byte
@@ -188,37 +194,62 @@ func (sf *File) AddField(name, label string, slice interface{}) *Field {
 		data:      slice,
 	}
 	sf.fields = append(sf.fields, fld)
-	sf.NoVar = int16(len(sf.fields))
-	if sliceLen > int(sf.NoObs) {
-		sf.NoObs = int32(sliceLen)
+	sf.NumVars = int16(len(sf.fields))
+	if sliceLen > int(sf.NumObs) {
+		sf.NumObs = int32(sliceLen)
 	}
 	return fld
 }
 
-//WriteTo writes the data to an io.Writer.
-//warning: the number of written byte is not used, always zero
+// AddFieldMeta adds a description of a field in a record
+func (sf *File) AddFieldMeta(name, label string, typ byte) *Field {
+	var (
+		format = "%9.0g"
+	)
+
+	switch typ {
+	case StataByteId:
+		sf.recordSize++ 
+	case StataIntId:
+		sf.recordSize += 2
+	case StataLongId:
+		sf.recordSize += 4
+	case StataFloatId:
+		sf.recordSize += 4
+	case StataDoubleId:
+		sf.recordSize += 8
+	default:
+		panic("unsupported data type in field " + name) //must be a programmer error, so panic
+	}
+	fld := &Field{
+		Name:      name,
+		FieldType: typ,
+		Label:     label,
+		Format:    format,
+	}
+	sf.fields = append(sf.fields, fld)
+	sf.NumVars = int16(len(sf.fields))
+	return fld
+}
+
+// WriteTo writes the data to an io.Writer.
+// warning: the number of written byte is not used, always zero
 func (sf *File) WriteTo(w io.Writer) (int64, error) {
-	if err := sf.writeHeader(w); err != nil {
-		return 0, err
-	}
-	if err := sf.writeDescriptors(w); err != nil {
-		return 0, err
-	}
 	return 0, sf.writeData(w)
 }
 
 func (sf *File) writeHeader(w io.Writer) error {
-	sf.NoVar = int16(len(sf.fields))
+	sf.NumVars = int16(len(sf.fields))
 	return binary.Write(w, littleEndian, *sf.header)
 }
 
 func (sf *File) writeDescriptors(w io.Writer) error {
-	sf.typList = make([]byte, sf.NoVar)
-	sf.varList = make([]stataVarName, sf.NoVar)
-	sf.srtList = make([]byte, 2*(sf.NoVar+1))
-	sf.fmtList = make([]stataFmtName, sf.NoVar)
-	sf.lblList = make([]stataVarName, sf.NoVar)
-	sf.vlblList = make([]stataLabel, sf.NoVar)
+	sf.typList = make([]byte, sf.NumVars)
+	sf.varList = make([]stataVarName, sf.NumVars)
+	sf.srtList = make([]byte, 2*(sf.NumVars+1))
+	sf.fmtList = make([]stataFmtName, sf.NumVars)
+	sf.lblList = make([]stataVarName, sf.NumVars)
+	sf.vlblList = make([]stataLabel, sf.NumVars)
 	for i, f := range sf.fields {
 		copy(sf.varList[i][:], f.Name) //only copy up to the size of stataVarName and pad with zeros
 		sf.typList[i] = f.FieldType
@@ -251,16 +282,17 @@ func (sf *File) writeDescriptors(w io.Writer) error {
 	return binary.Write(w, littleEndian, [5]byte{0, 0, 0, 0, 0})
 }
 
-//writeData loops over the field vectors and write their binary representation to an io.Writer
+// writeData loops over the field vectors and write their binary representation to an io.Writer
+// uses unsafe to  avoid using potentially slower binary.Write.
 func (sf *File) writeData(w io.Writer) error {
-	if sf.NoObs == 0 {
+	if sf.NumObs == 0 {
 		return nil
 	}
 	if len(sf.fields) == 0 {
 		return fmt.Errorf("No fields")
 	}
 	bs := make([]byte, sf.recordSize)
-	for i := int32(0); i < sf.NoObs; i++ {
+	for i := int32(0); i < sf.NumObs; i++ {
 		offset := 0
 		for _, f := range sf.fields {
 			switch f.FieldType {
@@ -297,8 +329,69 @@ func (sf *File) writeData(w io.Writer) error {
 	return nil
 }
 
-//FIXME: do not overwrite an existing file
-//WriteFile
+// BeginWrite must be called once after defining all fields and before writing records
+// fileName will be created or truncated if it already exists
+// it uses a 64kb buffer as recommended by Microsoft:
+// http://technet.microsoft.com/en-us/library/cc938632.aspx
+func (sf *File) BeginWrite(fileName string) error {
+	var err error
+	sf.f, err = os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	sf.w = bufio.NewWriterSize(sf.f, 64*1012) //use 64kb buffer
+
+	if err := sf.writeHeader(sf.w); err != nil {
+		return err
+	}
+	if err := sf.writeDescriptors(sf.w); err != nil {
+		return err
+	}
+	sf.recBuf = make([]byte, sf.recordSize)
+	sf.offset = 0
+	return nil
+}
+
+func (sf *File) EndWrite() error {
+	sf.w.Flush()
+	return sf.f.Close()
+}
+
+// RecordEnd must be called after writing all field data for the record
+func (sf *File) RecordEnd() error {
+	_, err := sf.w.Write(sf.recBuf)
+	sf.offset = 0
+	return err
+}
+
+func (sf *File) AppendByte(v Byte) {
+	sf.recBuf[sf.offset] = byte(v)
+	sf.offset++
+}
+func (sf *File) AppendInt(v Int) {
+	sf.recBuf[sf.offset] = byte(v)
+	sf.offset++
+	sf.recBuf[sf.offset] = byte(v >> 8)
+	sf.offset++
+}
+func (sf *File) AppendLong(v Long) {
+	base := *(*[4]byte)(unsafe.Pointer(&v)) //convert t to an equivalent byte array
+	copy(sf.recBuf[sf.offset:], base[:])
+	sf.offset += 4
+}
+func (sf *File) AppendFloat(v Float) {
+	base := *(*[4]byte)(unsafe.Pointer(&v))
+	copy(sf.recBuf[sf.offset:], base[:])
+	sf.offset += 4
+}
+func (sf *File) AppendDouble(v Double) {
+	base := *(*[8]byte)(unsafe.Pointer(&v))
+	copy(sf.recBuf[sf.offset:], base[:])
+	sf.offset += 8
+}
+
+// FIXME: do not overwrite an existing file
+// WriteFile
 func (sf *File) WriteFile(fileName string) error {
 	f, err := os.Create(fileName)
 	if err != nil {
